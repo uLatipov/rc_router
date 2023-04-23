6cca66b0eaf05b23dff7987ff89ed5dbccfd7e6c
Usmonxon Latipov <latipovusmonkhon2006@gmail.com>
2023-04-23 15:58:53 -0700

    Create pages & migrate to router v6

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
 root.render(
 	<React.StrictMode>
-		<Router>
-			<Route path="/" component={App} />
-		</Router>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
 
