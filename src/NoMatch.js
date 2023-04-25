6c0028e52e8ab17aa864b3c1220fa4ce4f85ac94
Usmonxon Latipov <latipovusmonkhon2006@gmail.com>
2023-04-24 18:54:13 -0700

    Complete about, nomatch pages

--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NoMatch = () => {
 	return (
-		<main>
-			<h1>NoMatch</h1>
+		<main className="Missing">
+			<h2>Page Not Found</h2>
+			<p>Well, that's disappointing.</p>
+			<p>
+				<Link to="/">Visit Our Homepage</Link>
+			</p>
 		</main>
 	);
 };
